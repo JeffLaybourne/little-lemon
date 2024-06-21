f79582c4945880f5743bc45c02668f853d95c17d
JeffLaybourne <64240582+JeffLaybourne@users.noreply.github.com>
2024-06-21 16:01:33 -0700

    chore: cleaned up imports for Home.kt and Profile.kt

--- app/src/main/java/com/example/littlelemon/screens/Profile.kt
+++ app/src/main/java/com/example/littlelemon/screens/Profile.kt
@@ -4,7 +4,6 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
