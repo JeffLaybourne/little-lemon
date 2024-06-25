58f1e32e9606dcfcd4e36238f67ca8d6d0030a7e
JeffLaybourne <64240582+JeffLaybourne@users.noreply.github.com>
2024-06-25 00:20:51 -0700

    feat: Added networking and Room dependencies. Made the network call to fetch the menu items.

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -5,9 +5,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import com.example.littlelemon.navigation.Navigation
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 lateinit var sharedPreferences: SharedPreferences
 
@@ -17,6 +27,11 @@ class MainActivity : ComponentActivity() {
 
         sharedPreferences = getSharedPreferences("Little Lemon", MODE_PRIVATE)
 
+        lateinit var fetchedItems: List<MenuItemNetwork>
+        lifecycleScope.launch(Dispatchers.IO) {
+            fetchedItems = fetchMenu()
+        }
+
         enableEdgeToEdge()
         setContent {
             LittleLemonTheme {
@@ -24,5 +39,24 @@ class MainActivity : ComponentActivity() {
                 Navigation(navController)
             }
         }
+    }
+
+    // TODO: the lines below will need to be updated for Room. They were filled in quickly
+    //  for network testing.
+    /**
+     * NETWORK REQUEST TO DATABASE
+     */
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        val response: MenuNetworkData = httpClient
+            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+            .body()
+
+        return response.menu
     }
 }
